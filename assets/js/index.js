11ff6afdbba0a5d04c9b4f1e7bc807bc703ddd73
Diana Sokolova <dianasokolova5843@gmail.com>
2021-03-31 17:33:22 +0300

    init commit

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+function Accumulator(startingValue){
+    return {
+        value: startingValue,
+        
+        read: function(){
+            this.value += +prompt('Введите число: ');
+        }
+    }
+}
+const value = +prompt('Введите 1е число: ');
+const result = Accumulator(value);
+const count = +prompt('Введите количество повторений: ');
+
+for(let i=0; i < count; i++){
+    result.read();
+}
+
+console.log('result: ', result.value);
